a3251525654ce3438f0100aef01f84ae1fa99e26
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-03-11 11:38:08 -0300

    Update app.ts

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,4 +1,8 @@
 import * as io from "socket.io";
+import * as tmi from "tmi.js";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 interface Product {
   title: string;
@@ -12,6 +16,37 @@ interface Vote {
   product: Product["key"];
   review?: string;
 }
+const options = {
+  options: {debug: true},
+  identity: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+  },
+  channels: ["juanma_piojoso"],
+};
+
+const client = new tmi.client(options);
+
+client.on("connected", (address, port) => {
+  console.log(`Hello ${address}:${port}`);
+});
+
+// tmi
+client.connect();
+
+client.on("chat", (target, ctx, msg, self) => {
+  if (self) return;
+
+  const commandName = msg.trim();
+
+  if (commandName === "!A") {
+    console.log("Hola Juanma desde Twitch");
+  }
+
+  if (commandName === "!B") {
+    console.log("Chau desde Twitch");
+  }
+});
 
 const server = new io.Server(5000, {
   cors: {
