900dbcf6065bcb6dce6c438c3ff13ebc0f71f496
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-03-10 23:33:42 -0300

    Update main.tsx

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {ChakraProvider} from "@chakra-ui/react";
 
-import HomeScreen from "./app/screens/Home";
+import {Provider as ServerProvider} from "~/context/ServerContext";
+
+import Home from "./app/screens/Home/Home";
 
 import "./theme.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HomeScreen />
+    <ServerProvider>
+      <ChakraProvider resetCSS>
+        <Home />
+      </ChakraProvider>
+    </ServerProvider>
   </React.StrictMode>,
   document.getElementById("root"),
 );
