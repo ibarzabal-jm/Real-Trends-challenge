2c83232cc09602433e3cc530c7ee9458ec2d579d
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-03-10 23:35:08 -0300

    Update app.ts

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,11 +1,56 @@
 import * as io from "socket.io";
 
+interface Product {
+  title: string;
+  description: string;
+  image: string;
+  key: string;
+}
+
+interface Vote {
+  user: string;
+  product: Product["key"];
+  review?: string;
+}
+
 const server = new io.Server(5000, {
   cors: {
     origin: "http://localhost:3000",
   },
 });
 
+const products: Product[] = [
+  {
+    key: "A",
+    description: "Product A Description",
+    title: "Product A title",
+    image: "//placehold.it/256x256",
+  },
+  {
+    key: "B",
+    description: "Product B Description",
+    title: "Product B title",
+    image: "//placehold.it/256x256",
+  },
+];
+const votes: Vote[] = [
+  {
+    user: "Juanma",
+    product: "A",
+    review: "Horrible",
+  },
+  {
+    user: "Juanma",
+    product: "B",
+    review: "Feo",
+  },
+  {
+    user: "Juanma",
+    product: "B",
+    review: "Hermoso",
+  },
+];
+
 server.on("connection", (socket) => {
-  socket.emit("ping", "pong");
+  socket.emit("state", {products, votes});
 });
