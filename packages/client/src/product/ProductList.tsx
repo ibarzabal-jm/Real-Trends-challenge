6fbecb05a9df711a860e90663f0d8ce95a56b2d0
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-03-11 11:38:37 -0300

    Update ProductList.tsx

--- packages/client/src/product/ProductList.tsx
+++ packages/client/src/product/ProductList.tsx
@@ -17,13 +17,27 @@ const ProductList: React.FC<Props> = ({products, votes}) => {
     <Stack isInline>
       {products.map((product) => {
         const productVotes = votes.filter((votes) => votes.product === product.key);
+        const percentage = (productVotes.length * 100) / votes.length;
 
         return (
-          <Stack key={product.key}>
-            <Heading>{product.title}</Heading>
-            <Image maxH={256} maxW={256} rounded={8} src={product.image} />
-            <Text>{product.description}</Text>
-            <VotesList votes={productVotes} />
+          <Stack key={product.key} as="section" padding={4} position="relative">
+            <Stack bg="white" borderRadius="20px" position="relative" zIndex={1}>
+              <Heading>{product.title}</Heading>
+              <Image maxH={256} maxW={256} rounded={8} src={product.image} />
+              <Text>{product.description}</Text>
+              <VotesList votes={productVotes} />
+            </Stack>
+            <Stack
+              bg={`hsl(${percentage}, 100%, 50%)`}
+              borderBottomRadius={8}
+              bottom={0}
+              height={`${percentage}%`}
+              left={0}
+              position="absolute"
+              transition="1s"
+              width="100%"
+              zIndex={0}
+            />
           </Stack>
         );
       })}
